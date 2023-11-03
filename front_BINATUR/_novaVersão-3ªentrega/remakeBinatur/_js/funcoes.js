e4b9a410db4e1c2d0eebbfea2b28714f3b482647
Squad16-RECODE2023 <squad16.recode2023@gmail.com>
2023-11-02 22:32:39 -0300

    Adicionado uma modal (alerta) que direciona o usuário para efetuar o cadastro.

--- front_BINATUR/_novaVersão-3ªentrega/remakeBinatur/_js/funcoes.js
+++ front_BINATUR/_novaVersão-3ªentrega/remakeBinatur/_js/funcoes.js
@@ -1,3 +1,28 @@
+// Lógica para o btn do modal direcionar para a page cadastro:
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecione o botão pelo id
+    var modalBtn = document.getElementById('modalBtn');
+  
+    // Adicione um ouvinte de evento de clique
+    modalBtn.addEventListener('click', function() {
+      // Redirecione o usuário para a página desejada, por exemplo:
+      window.location.href = '_html/cadastro.html';
+    });
+  });
+//Fim - Lógica para o btn do modal direcionar para a page cadastro. =================================================================== 
+
+//Lógica para o funcionamento do modal (alerta de cadastro): ========================================================================== 
+document.addEventListener('DOMContentLoaded', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
+        keyboard: false
+    });
+    setTimeout(function () {
+        myModal.show();
+    }, 2000);
+});
+//Fim - Lógica para o funcionamento do modal (alerta de cadastro): =================================================================== 
+
+// Lógica para o funcionamento troca de nacionalidade (alternância dos cards promocionais): ===========================================
 document.addEventListener("DOMContentLoaded", function () {
     // Seleciona os botões e as divs
     var btnNacional = document.getElementById("btnNacional");
@@ -21,31 +46,20 @@ document.addEventListener("DOMContentLoaded", function () {
     destinoNacional.style.display = "block";
     destinoInternacional.style.display = "none";
 });
-
-
-
-
-
-
-
-
-
-
-
+//Fim -  Lógica para o funcionamento troca de nacionalidade (alternância dos cards promocionais). =====================================
 
 //Lógica para a transição dos botões de nacionalidade: ================================================================================
-
 document.getElementById("btnNacional").classList.add("active"); // Adiciona a classe 'active' ao botão Nacional por padrão
 
 // Adiciona o evento de clique aos botões
-document.getElementById("btnNacional").addEventListener("click", function() {
+document.getElementById("btnNacional").addEventListener("click", function () {
     document.getElementById("btnNacional").classList.add("active");
     document.getElementById("btnInternacional").classList.remove("active");
 });
 
 // Adiciona o evento de clique aos botões
-document.getElementById("btnInternacional").addEventListener("click", function() {
+document.getElementById("btnInternacional").addEventListener("click", function () {
     document.getElementById("btnInternacional").classList.add("active");
     document.getElementById("btnNacional").classList.remove("active");
 });
-//Fim - Lógica para a transição dos botões de nacionalidade: ==========================================================================
+//Fim - Lógica para a transição dos botões de nacionalidade. ==========================================================================
