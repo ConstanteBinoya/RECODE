7689b46f88e25312c5122219c565636f34cc1cf5
Squad16-RECODE2023 <squad16.recode2023@gmail.com>
2023-12-18 18:31:51 -0300

    Interface mais agradável ao usuário

--- _4ªentrega/back_spring-boot/springdata/binatur_cadastro-cliente/src/main/java/com/example/binatur_db/dtos/ClientRecordDto.java
+++ _4ªentrega/back_spring-boot/springdata/binatur_cadastro-cliente/src/main/java/com/example/binatur_db/dtos/ClientRecordDto.java
@@ -1,17 +1,3 @@
-//package com.example.binatur_db.dtos;
-//
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-//
-//import java.math.BigDecimal;
-//
-////public record ClientRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
-////}
-//
-//public record ClientRecordDto(@NotBlank String name, @NotNull String cpf) {
-//}
-
-
 package com.example.binatur_db.dtos;
 
 import jakarta.validation.constraints.NotBlank;
--- _4ªentrega/back_spring-boot/springdata/binatur_cadastro-cliente/src/main/java/com/example/binatur_db/repositorys/ClientRepository.java
+++ _4ªentrega/back_spring-boot/springdata/binatur_cadastro-cliente/src/main/java/com/example/binatur_db/repositorys/ClientRepository.java
@@ -1,15 +1,3 @@
-//package com.example.binatur_db.repositorys;
-//
-//import com.example.binatur_db.models.ClientModel;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.UUID;
-//
-//@Repository
-//public interface ClientRepository extends JpaRepository<ClientModel, UUID> {
-//}
-
 package com.example.binatur_db.repositorys;
 
 import com.example.binatur_db.models.ClientModel;
